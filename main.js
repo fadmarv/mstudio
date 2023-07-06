97bad1a2c02676b729fadb6c68898c803936e802
fadmarv <132268184+fadmarv@users.noreply.github.com>
2023-07-06 13:47:09 +0100

    my commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// var Name = document.getElementById("inp");
+// var sel = document.getElementById("sel");
+// var submit = document.getElementById("submit");
+// var result = document.getElementById("result");
+
+// function res() {
+//     result.innerHTML = "Thanks for filling the form" + Name
+// }
+var in_p = document.getElementById("inp")
+var in_p1 = document.getElementById("inp1")
+function change() {
+    // console.log(in_p.value)
+
+//    if (in_p.value == "boy") {
+//     console.log("hello")
+//    }
+//    else if (in_p.value = "girl" ) {
+//     console.log("input in value")
+//    }
+//    else {
+//     console.log("error")
+//    }
+}
+
+function res(x) {
+    in_p.value = x
+}
+
+
+
